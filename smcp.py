b88a080e59794e64181ce09d1b79c71ec1f6e9ec
sofiatabioye <sofiat.abioye@previsico.comgit config --global user.email sofiat.abioye@previsico.com>
2023-02-26 14:54:33 +0000

    update data extraction classes

--- smcp.py
+++ smcp.py
@@ -13,24 +13,29 @@ def makeSoup(limit, offset, alphabet):
 
 # get all data on page with data_row class
 def getPageData(content):
-    data_rows = content.find_all("div", {"class": "data-row"})
+    data_rows = content.find_all("div", {"class": "search-results-product"})
     page_dataset = []
 
     for row in data_rows:
-        drug_name = row.div.h2.a.string
-        company_name = row.div.h4.a.string
-        smcp_link = "https://www.medicines.org.uk" + row.div.h2.a.get('href')
+        drug_name = row.find("div", {"class": "search-results-product-info-name"}).a.text;
+        company_name = row.find("div", {"class": "search-results-product-info-company"}).a.text;
+        links = row.find("ul", {"class": "search-results-product-links"})
+        smpc = links.find("a", string= "Health Professionals (SmPC)");
+        smpc_link =  "";
+        if(smpc is not None):
+            smpc_link = "https://www.medicines.org.uk" + smpc['href'];
         page_dataset.append({
-            "drug_name": drug_name,
-            "company_name": company_name,
-            "smcp_link": smcp_link
+            "drug_name": drug_name.strip(),
+            "company_name": company_name.strip(),
+            "smpc_link": smpc_link
         })
     return page_dataset
 
 
 def getAllDrugs():
     alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    dataset = []
+    # alphabets = ['A']
+    dataset = {}
 
     for alphabet in alphabets:
         offset = 1
@@ -40,10 +45,13 @@ def getAllDrugs():
         page_data = []
         # get pagination data
         soup = makeSoup(offset=offset, limit=limit, alphabet=alphabet)
-        pagination = soup.find("ul", {"class": "search-panel-paging"})
-        pagination_string = pagination.li.string
-        page_numbers = re.findall('[0-9]+', pagination_string)
-        highest_page_number += int(page_numbers[1]) - 1;
+        pagination = soup.find("ul", {"class": "search-panel-paging"});
+        if(pagination is not None):
+            pagination = pagination.find("li", {"class": "text-presentation"}).text;
+            page_numbers = re.findall('[0-9]+', pagination)
+            highest_page_number = int(page_numbers[1]);
+            # number_of_pages = highest_page_number
+    
 
         # get first page data
         content = soup.find("main", {"class": "main-home"})
@@ -58,12 +66,12 @@ def getAllDrugs():
                 offset+=limit
                 soup = makeSoup(offset=offset, limit=limit, alphabet=alphabet)
                 number_of_pages = number_of_pages + 1
-                content = soup.find("main", {"class": "main-home"})
                 page_dataset = getPageData(content);
                 page_data.extend(page_dataset);
                 # .append({alphabet: page_dataset})
                 print(alphabet, number_of_pages, offset, limit)
-        dataset.append({alphabet: page_data})
+        # dataset.append({alphabet: page_data})
+        dataset[alphabet] = page_data
 
     # write dataset to file
     with open('smcp_drugs_list3.json', 'wt') as outfile:
